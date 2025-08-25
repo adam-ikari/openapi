015888b0223fd9c66268f461f6f8f6e3256231b8
adam-ikari <adam-ikari@hotmail.com>
2025-08-25 19:21:44 +0800

    build: 更新构建脚本和目录结构
    - 修改 package.json 中的 build:doc 脚本，使用相对路径指向 build_doc.js
    - 删除根目录下的 build.sh 脚本
    - 将 build_doc.js 从根目录移动到 scripts 目录

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tsp compile src/main.tsp
-widdershins \
-    --summary tsp-output/schema/openapi.yaml \
-    --search false \
-    --language_tabs 'javascript:Javascript' 'python:Python' \
-    -o doc/openapi.md
